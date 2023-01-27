c86067ba47184871fa12e4e0027be52c1f95f0f3
Syrinthia Swift <116780275+sswift1995@users.noreply.github.com>
2023-01-26 22:39:19 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 class Casino {
-    // Write code here
-};
+    constructor(){
+        this.timesPlayed = 0;
+    }
+    playGame(betAmount) {
+        this.timesPlayed += 1;
+        let outcome = Math.random();
+        if (outcome <= 0.5) {
+            console.log("The casino wins!");
+        } else {
+            let winnings = betAmount * (this.timesPlayed + 1);
+            console.log(`Player wins ${winnings}!`);
+        }
+    }
+}
+
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
+myCasino.playGame(10);// Player wins 20! or "The casino wins!"
+myCasino.playGame(10);  // Player wins 30! or "The casino wins!"
+myCasino.playGame(10);
 myCasino.playGame(25);
 myCasino.playGame(35);
 
